9d5baefa44dafe5c03a00c223747122aeffa2617
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-15 07:58:30 +0200

    reusability pt. 1

--- src/app/pages/home/components/dropdown-menu/dropdown-menu.component.ts
+++ src/app/pages/home/components/dropdown-menu/dropdown-menu.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dropdown-menu',
-  templateUrl: './dropdown-menu.component.html',
-  styleUrls: ['./dropdown-menu.component.css']
+    selector: 'app-dropdown-menu',
+    templateUrl: './dropdown-menu.component.html',
+    styleUrls: ['./dropdown-menu.component.css'],
 })
 export class DropdownMenuComponent {
+    // sorting
+    options: { label: string; checked: boolean }[] = [
+        { label: 'Ascending', checked: true },
+        { label: 'Descending', checked: false },
+    ];
+    selectedOption: string = '';
+    isListVisible: boolean = false;
 
+    toggleSortingList(): void {
+        this.isListVisible = !this.isListVisible;
+    }
+
+    selectOption(option: { label: string; checked: boolean }) {
+        this.options.forEach((opt) => {
+            if (opt === option) {
+                opt.checked = true;
+                this.selectedOption = opt.label;
+            } else {
+                opt.checked = false;
+            }
+        });
+    }
+
+    // filtering
+    toggleFilteringList(): void {
+        this.isListVisible = !this.isListVisible;
+    }
+
+    // layout
+    toggleLayoutList(): void {
+        this.isListVisible = !this.isListVisible;
+    }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,42 +5,4 @@ import { Component } from '@angular/core';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-    // sorting
-    options: { label: string; checked: boolean }[] = [
-        { label: 'Ascending', checked: true },
-        { label: 'Descending', checked: false },
-    ];
-    selectedOption: string = 'Ascending';
-    isListVisible: boolean = false;
-
-    toggleSortingList(): void {
-        this.isListVisible = !this.isListVisible;
-    }
-
-    selectOption(option: { label: string; checked: boolean }) {
-        this.options.forEach((opt) => {
-            if (
-                option.checked === true &&
-                option.label === this.selectedOption
-            ) {
-                return;
-            } else if (opt === option) {
-                opt.checked = true;
-                this.selectedOption = opt.label;
-            } else {
-                opt.checked = false;
-            }
-        });
-    }
-
-    // filtering
-    toggleFilteringList(): void {
-        this.isListVisible = !this.isListVisible;
-    }
-
-    // layout
-    toggleLayoutList(): void {
-        this.isListVisible = !this.isListVisible;
-    }
-}
+export class HomeComponent {}
